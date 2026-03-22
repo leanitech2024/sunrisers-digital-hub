4f876b6ece484854ea294cddc2536a4039516c3f
Abhijit47 <akarmakar846@gmail.com>
2026-03-22 14:43:08 +0530

    feat: add robots and sitemap generation for SEO optimization

---
+++ src/app/sitemap.ts
@@ -0,0 +1,26 @@
+import { allBlogs } from 'content-collections';
+import type { MetadataRoute } from 'next';
+
+const BASE_URL = process.env.BASE_URL || 'http://localhost:3000';
+
+const blogSlugs = allBlogs.map((post) => post.slug);
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: `${BASE_URL}`,
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1,
+    },
+    ...blogSlugs.map(
+      (slug) =>
+        ({
+          url: `${BASE_URL}/blog/${slug}`,
+          lastModified: new Date(),
+          changeFrequency: 'monthly',
+          priority: 0.8,
+        }) as MetadataRoute.Sitemap[0],
+    ),
+  ];
+}
